55a9d403db1276199eee82b5634d7e47b875ae33
mavs0890 <mavs0890@gmail.com>
2019-07-10 17:14:13 -0600

    setup webapi project with swagger

---
+++ Startup.fs
@@ -0,0 +1,47 @@
+namespace fsharp_jumpstart_workshop
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.HttpsPolicy;
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.OpenApi.Models
+open Swashbuckle.AspNetCore.Swagger
+
+type Startup private () =
+    new (configuration: IConfiguration) as this =
+        Startup() then
+        this.Configuration <- configuration
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    member this.ConfigureServices(services: IServiceCollection) =
+        // Add framework services.
+        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2) |> ignore
+
+        let info = OpenApiInfo()
+        info.Title <- "My API V1"
+        info.Version <- "v1"
+        services.AddSwaggerGen(fun c -> c.SwaggerDoc("v1", info)) |> ignore
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
+        if (env.IsDevelopment()) then
+            app.UseDeveloperExceptionPage() |> ignore
+        else
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts() |> ignore
+
+        app.UseSwagger() |> ignore
+        app.UseSwaggerUI( fun c -> 
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Workshop API V1") |> ignore
+            c.RoutePrefix = "" |> ignore
+        ) |> ignore
+        app.UseHttpsRedirection() |> ignore
+        app.UseMvc() |> ignore
+
+    member val Configuration : IConfiguration = null with get, set
