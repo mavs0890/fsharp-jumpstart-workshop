de5d747b96b7149bbee02fc902d4ddca85222ad2
mavs0890 <mavs0890@gmail.com>
2019-07-17 13:56:00 -0600

    validateEmailTest Passing

--- fsharp-jumpstart-workshop/Logic/Validation.fs
+++ fsharp-jumpstart-workshop/Logic/Validation.fs
@@ -5,4 +5,5 @@ open fsharp_jumpstart_workshop.Types
 
 module Validation = 
     let validateEmail (email : string) : bool =
-        failwith "not-implemented"
+        let splitEmail = email.Split("@")
+        email.Length > 2 && (splitEmail.Length - 1) = 1 && (splitEmail |> Array.filter (fun (s : string) -> s.Length = 0)).Length = 0
