8bc22fabcdcfb77f35d0734b743792d1907e1b38
mavs0890 <mavs0890@gmail.com>
2019-07-17 13:27:00 -0600

    add tests failing tests for email validation

---
+++ Tests/ValidationTests.fs
@@ -0,0 +1,80 @@
+namespace Tests
+
+open System
+open Xunit
+open Swensen.Unquote
+open fsharp_jumpstart_worskhop.Logic
+open fsharp_jumpstart_workshop.Types
+
+module ValidationTests =
+
+    let private createTestMemberOnPlan (planId : string) : Member = {
+        Id = Random().Next()
+        FirstName = "FirstName_" + (Guid.NewGuid() |> string)
+        LastName = "LastName_" + (Guid.NewGuid() |> string)
+        Email = (Guid.NewGuid() |> string) + "@test.com"
+        PlanId = planId
+    }
+
+    [<Fact>]
+    let ``validateEmail will verify email is atleast 3 characters `` () = 
+        //setup
+        let invalidEmail = "ab"
+        let validEmail = "a@b"
+        let validEmailLong = "abcdefghijklnopqrtuvwxyz@abcdefghijklnopqrtuvwxyz"
+
+        //execute
+        let resultForInvalidEmail = Validation.validateEmail invalidEmail
+        let resultForValidEmail = Validation.validateEmail validEmail
+        let resultForValidEmailLong = Validation.validateEmail validEmailLong
+
+        //verify
+        test <@ not resultForInvalidEmail @>
+        test <@ resultForValidEmail @>
+        test <@ resultForValidEmailLong @>
+
+    [<Fact>]
+    let ``validateEmail will verify email contains at symbol`` () = 
+        //setup
+        let invalidEmail = "abcdefg"
+        let validEmail = "abcdefg@abcdefg"
+
+        //execute
+        let resultForInvalidEmail = Validation.validateEmail invalidEmail
+        let resultForValidEmail = Validation.validateEmail validEmail
+
+        //verify
+        test <@ not resultForInvalidEmail @>
+        test <@ resultForValidEmail @> 
+
+    [<Fact>]
+    let ``validateEmail will verify email only contains at symbol once`` () =
+        //setup
+        let invalidEmail = "abc@abc@abc"
+        let validEmail = "abc@abc"   
+
+        //execute
+        let resultForInvalidEmail = Validation.validateEmail invalidEmail
+        let resultForValidEmail = Validation.validateEmail validEmail
+
+        //verify
+        test <@ not resultForInvalidEmail @>
+        test <@ resultForValidEmail @>
+
+    [<Fact>]
+    let ``validateEmail will verify that there is text before and after the at symbol`` () =
+        //setup
+        let invalidEmail = "abc@"
+        let invalidEmail2 = "@abc"
+        let validEmail = "abc@abc"  
+
+        //execute
+        let resultForInvalidEmail = Validation.validateEmail invalidEmail
+        let resultForValidEmail2 = Validation.validateEmail invalidEmail2
+        let resultForValidEmail = Validation.validateEmail validEmail
+
+        //verify
+        test <@ not resultForInvalidEmail @>
+        test <@ not resultForValidEmail2 @>
+        test <@ resultForValidEmail @>
+
---
+++ fsharp-jumpstart-workshop/Logic/Validation.fs
@@ -0,0 +1,8 @@
+namespace fsharp_jumpstart_worskhop.Logic
+
+open System
+open fsharp_jumpstart_workshop.Types
+
+module Validation = 
+    let validateEmail (email : string) : bool =
+        failwith "not-implemented"
